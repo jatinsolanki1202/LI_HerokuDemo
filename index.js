5986d3fdb6237b4a1542a04ae01a15be7e0afa75
jatinsolanki <jatinsolanki1202@gmail.com>
2025-01-31 17:40:37 +0530

    refactor views path in vercel.json

--- index.js
+++ index.js
@@ -3,10 +3,12 @@ require('dotenv').config()
 const cookieParser = require('cookie-parser')
 const userRouter = require('./routes/user.routes.js')
 const dbConnection = require('./config/db.js')
+const path = require('path');
 
 dbConnection()
 const app = express()
 
+app.set('views', path.join(__dirname, 'views'));
 app.set("view engine", "ejs")
 
 app.use(express.json())
